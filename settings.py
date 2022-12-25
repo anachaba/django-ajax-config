4504b109f68d047f9ec4d9d38dce2bedc938567d
anach <54176002+codeZenon@users.noreply.github.com>
2022-12-25 22:07:12 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'mssql',
+        'NAME': 'name_django',
+        'USER': 'rootuser',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+            'driver': 'ODBC Driver 17 for SQL Server',
+        }
+    }
+}
